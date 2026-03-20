d22c4d3f5a758bdf76ee0daeefbffc699c9566b1
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2026-03-20 09:28:03 +0100

    eJERCICIO DE PERSISTENCIAS Y BASES DE DATOS

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url="jdbc:oracle:thin:@localhost:1521:xe";
+        String user="RIBERA";
+        String password="ribera";
+
+        try (Connection conn = DriverManager.getConnection(url,user,password)){
+            String sql="UPDATE empleado2 SET salario=? WHERE nombre=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setDouble(1, 4500.0);
+            preparedStatement.setString(2, "Riber");
+            int filas = preparedStatement.executeUpdate();
+            System.out.println("Filas actualizadas: " + filas);
+        }catch (SQLException e){
+            System.out.println("ERROR: "+e.getMessage());
+        }
+    }
+}
